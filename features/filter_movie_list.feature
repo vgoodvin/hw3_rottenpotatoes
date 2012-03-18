77dbc89fa554c69941a39c5e603cc76f25bfdab5
Vasily Gudoshnikov <vasily@shvetsgroup.com>
2012-03-18 17:00:45 +0400

    Feature "filter_movie_list"

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,35 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  When I check the following ratings: PG, R
+  And I uncheck the following ratings: PG-13, NC-17, G
+  #When I check "ratings[PG]"
+  #And I check "ratings[R]"
   # enter step(s) to uncheck all other checkboxes
+  #And I uncheck "ratings[PG-13]"
+  #And I uncheck "ratings[NC-17]"
+  #And I uncheck "ratings[G]"
   # enter step to "submit" the search form on the homepage
+  And I press "Refresh"
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should be on the RottenPotatoes home page
+  And I should see "The Incredibles"
+  And I should see "Raiders of the Lost Ark"
+  And I should see "Amelie"
+  And I should see "The Terminator"
+  And I should see "When Harry Met Sally"
   # enter step(s) to ensure that other movies are not visible
+  And I should not see "Aladdin"
+  And I should not see "The Help"
+  And I should not see "Chocolat"
+  And I should not see "2001: A Space Odyssey"
+  And I should not see "Chicken Run"
 
 Scenario: no ratings selected
   # see assignment
 
 Scenario: all ratings selected
-  # see assignment
+  When I check the following ratings: PG, R, PG-13, NC-17, G
+  And I press "Refresh"
+  Then I should be on the RottenPotatoes home page
+  And I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,14 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+  Movie.delete_all
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create!(
+        :title => movie['title'],
+        :rating => movie['rating'],
+        :release_date => movie['release_date']
+    )
   end
-  assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -17,12 +20,26 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   assert false, "Unimplmemented"
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(',').each { |rating| rating.strip! }
+  ratings.each do |rating|
+    step %Q{I #{uncheck}check "ratings_#{rating}"}
+  end
+end
+
+Then /I should see all of the movies/ do
+  assert page.has_css?("table#movies tbody tr", :count => Movie.count)
+end
+
+Given /I have added "(.*)" with rating "(.*)"/ do |title, rating|
+  Given %Q{I am on the Create New Movie page}
+  When  %Q{I fill in "Title" with "#{title}"}
+  And   %Q{I select "#{rating}" from "Rating"}
+  And   %Q{I press "Save Changes"}
+end
+
+Then /I should see "(.*)" before "(.*)" on (.*)/ do |string1, string2, path|
+  Given %Q{I am on #{path}}
+  regexp = Regexp.new ".*#{string1}.*#{string2}"
+  page.body.should =~ regexp
 end
